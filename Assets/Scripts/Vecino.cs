978c647d1a874dd433909bb3e096494481482506
Fran Gambero <frangansal.13@gmail.com>
2020-04-25 20:22:09 +0200

    fix: sprites con buena posicion

--- Assets/Scripts/Vecino.cs
+++ Assets/Scripts/Vecino.cs
@@ -4,22 +4,27 @@ using UnityEngine;
 
 public class Vecino : MonoBehaviour
 {
+    private Transform hall;
     private Transform habitacion;
+    public bool hasHabitacion;
 
 
     private void Awake() {
+        hall = PlayerController.Instance.positions[0, 1];
         Debug.Log("Me despertaste wei");
+        hasHabitacion = false;
         assignRoom(Random.Range(1, 3));
     }
     public void assignRoom(int nextHabitacion) {
-        Debug.Log("La habias" + nextHabitacion);
         habitacion = PlayerController.Instance.positions[nextHabitacion, 0];
+        Debug.Log("La habias" + habitacion.position);
         transform.position = habitacion.position;
+        hasHabitacion = true;
     }
 
     private void Update() {
-        if (habitacion) {
-            //transform.Translate(habitacion.position * Time.deltaTime * 2);
+        if (hasHabitacion) {
+                                     // bruh
         }
     }
 }
