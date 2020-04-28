d336d2ffaf9e13556d60404a7a76bd58d990a670
JesusLX <jpmx96@gmail.com>
2020-04-28 21:20:03 +0200

    Edificio Creciente

---
+++ Assets/Scripts/GrowBuilding.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GrowBuilding : Singleton<GrowBuilding> {
+    public List<Vector3> positions;
+    Vector3 nextPostion;
+    public ShakeTransformS cameraShaker;
+    public List<ParticleSystem> dustParticles;
+    public bool IsMoving { get; private set;}
+    public int CurrentFloor { get; private set;}
+    public Action BuildStopMovingAction;
+    public Action BuildStartMovingAction;
+
+    #region TEST
+    [ContextMenu("TEST Move To 0")]
+    public void TESTMoveTo0() {
+        SetInPostion(0);
+    }
+    [ContextMenu("TEST Move To 1")]
+    public void TESTMoveTo1() {
+        SetInPostion(1);
+    }
+    [ContextMenu("TEST Move To 2")]
+    public void TESTMoveTo2() {
+        SetInPostion(2);
+    }
+    [ContextMenu("TEST Move To 3")]
+    public void TESTMoveTo3() {
+        SetInPostion(3);
+    }
+    #endregion
+
+    private void Awake() {
+        dustParticles.ForEach(o => o.Stop());
+    }
+
+    public void MoveNextFloor() {
+        if(CurrentFloor <= positions.Count - 1) {
+
+        }
+    }
+
+    public void SetInPostion(int indexPosition) {
+        nextPostion = positions[indexPosition];
+        StartCoroutine(MoveToNextPositon());
+        CurrentFloor = indexPosition;
+    }
+
+    private IEnumerator MoveToNextPositon() {
+        IsMoving = true;
+        OnBuildStartMoving();
+        cameraShaker.Begin();
+        dustParticles.ForEach(o => o.Play());
+        while (Vector3.Distance(transform.position, nextPostion) > 0) {
+            transform.position = Vector3.MoveTowards(transform.position, nextPostion, Time.deltaTime);
+            yield return new WaitForSeconds(.01f);
+        }
+        dustParticles.ForEach(o => o.Stop());
+        cameraShaker.Stop();
+        transform.position = nextPostion;
+        IsMoving = false;
+    }
+
+    public void OnBuildStartMoving() {
+        if (BuildStartMovingAction != null) {
+            BuildStartMovingAction();
+        }
+    }public void OnBuildStopMoving() {
+        if (BuildStopMovingAction != null) {
+            BuildStopMovingAction();
+        }
+    }
+}
---
+++ Assets/Scripts/ShakeTransformS.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShakeTransformS : MonoBehaviour {
+    [Header("Info")]
+    private Vector3 _startPos;
+    private bool _doSake = false;
+    private Vector3 _randomPos;
+
+    [Header("Settings")]
+    [Range(0f, 2f)]
+    public float _time = 0.2f;
+    [Range(0f, 2f)]
+    public float _distance = 0.1f;
+    [Range(0f, 0.1f)]
+    public float _delayBetweenShakes = 0f;
+    public bool IsSaking {
+        get { return _doSake; }
+    }
+    private void Awake() {
+        _startPos = transform.position;
+    }
+
+    private void OnValidate() {
+        if (_delayBetweenShakes > _time)
+            _delayBetweenShakes = _time;
+    }
+    /// <summary>
+    /// Start the shake
+    /// </summary>
+    public void Begin() {
+        StopAllCoroutines();
+        _doSake = true;
+        StartCoroutine(Shake());
+    }
+    /// <summary>
+    /// Stop shake
+    /// </summary>
+    public void Stop() {
+
+        _doSake = false;
+        StopAllCoroutines();
+
+    }
+    /// <summary>
+    /// Shake the transform
+    /// </summary>
+    /// <returns></returns>
+    private IEnumerator Shake() {
+
+        while (_doSake) {
+            _randomPos = _startPos + (Random.insideUnitSphere * _distance);
+
+            transform.position = _randomPos;
+
+            if (_delayBetweenShakes > 0f) {
+                yield return new WaitForSeconds(_delayBetweenShakes);
+            } else {
+                yield return null;
+            }
+        }
+
+        transform.position = _startPos;
+    }
+
+}
