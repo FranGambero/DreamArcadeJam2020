3478fb20dfd920ac55864c24869b085f03fa9eb2
Fran Gambero <frangansal.13@gmail.com>
2020-04-29 22:05:29 +0200

    feat: UI Controller pa' la pausa

---
+++ Assets/Scripts/UIController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIController : MonoBehaviour
+{
+    public GameObject PauseMenu;
+    private bool isPaused = false;
+
+    private void Awake() {
+        PauseMenu.SetActive(false);
+    }
+    private void Update() {
+        if (Input.GetKeyDown(KeyCode.Escape)) {
+            setPause();
+        }
+    }
+
+    private void setPause() {
+        isPaused = !isPaused;
+        if (isPaused) {
+            Time.timeScale = 0;
+        } else {
+            Time.timeScale = 1;
+        }
+            PauseMenu.SetActive(isPaused);
+    }
+}
